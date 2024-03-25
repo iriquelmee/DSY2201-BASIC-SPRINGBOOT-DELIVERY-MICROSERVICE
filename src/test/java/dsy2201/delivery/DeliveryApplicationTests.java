20048dc4325e3cb90ae484601fbea80cd88e5939
iriquelmee <i.riquelmee@duocuc.cl>
2024-03-24 21:10:29 -0300

    Commit inicial - Estructura basica microservicio DSY2201 Sumativa 1 IGNACIO EDUARDO RIQUELME ERAZO.

---
+++ src/test/java/dsy2201/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dsy2201.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
